8f1a1dc6016f9490726801d5756e6b6a2c1ab173
jroaperedo <jroaperedo@gmail.com>
2024-09-20 07:26:25 -0600

    Added the scoreboard, and a message detailing the outcome. Could've made the message more detailed (e.g Rock beats Scissors..) but didn't feel like playing with the code much further.

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
+    //user makes a selection
+    //selection gets stored in humanChoice? or, run playround function as a callback
+    
+    //playRound();
+    //fetch computerChoice
+    //compare choices, determine a winner
+    //update scoreboard
+        //scoreboard should have a playerScore and computerScore
+    //if either score is 5, end game. Output the winner. 
+    //reset scoreboard.
+    //else, prompt user to make another choice.
 
+
+//Link buttons to humanChoice
+const rock=document.querySelector("#rock");
+const paper=document.querySelector("#paper");
+const scissors=document.querySelector("#scissors");
+
+let humanScore = 0;
+let computerScore = 0;
+
+//Scoreboard
+const list = document.querySelector("ul");
+const playerDisplay = document.querySelector("#playerScore");
+const computerDisplay = document.querySelector("#computerScore");
+const message = document.querySelector("#message");
+
+list.style.listStyle = "none";
+
+
+rock.addEventListener("click",()=>{
+    let humanChoice = "rock";
+    playRound(humanChoice);
+});
+
+paper.addEventListener("click",()=>{
+    let humanChoice = "paper";
+    playRound(humanChoice);
+});
+
+scissors.addEventListener("click",()=>{
+    let humanChoice = "scissors";
+    playRound(humanChoice);
+});
+
+//computer choice function
 function getComputerChoice() {
     
     let x = Math.floor(Math.random() * 3); //random number generator multiplied by max number of options (3 = paper, rock, scissors)
@@ -17,116 +62,63 @@ function getComputerChoice() {
     return y;
 }
 
-//Link buttons to humanChoice
-const rock=document.querySelector("#rock");
-const paper=document.querySelector("#paper");
-const scissors=document.querySelector("#scissors");
+//let's play a round
+function playRound(humanChoice) {
+    let result = "";
+    let computerChoice = getComputerChoice();
 
-rock.addEventListener("click",()=>{
-    let humanChoice = "rock";
-    console.log(humanChoice);
-});
-
-paper.addEventListener("click",()=>{
-    let humanChoice = "paper";
-    console.log(humanChoice);
-});
-
-scissors.addEventListener("click",()=>{
-    let humanChoice = "scissors";
-    console.log(humanChoice);
-});
-
-
-//function getHumanChoice() {
-//    let userChoice = "null"
-//    let userInput = prompt("Pick Rock, Paper, or Scissors");
-//    userChoice = userInput.toLowerCase();
-//    return userChoice;
-//}
-
-function playGame(){
-
-    let playerScore = 0;
-    let computerScore = 0;
-    let draw = 0;
-    let scoreboard = "";
-    
-    //for (let round = 1; round<6;round++){
-    //let humanChoice1 = getHumanChoice();
-    //let computerChoice1 = getComputerChoice(); 
-    //let result = playRound(humanChoice1, computerChoice1);
-    //scoreboard = "Round " + round + ": " + playerScore + " Player | " + computerScore + " Computer | " + draw + " draws";
-    //console.log(result, scoreboard);
-    //}
-
-    //user makes a selection
-    //selection gets stored in humanChoice
-    //fetch computerChoice
-    //compare choices, determine a winner
-    //update scoreboard
-    //once either hits 5 wins, announce a winner and reset the score
-
-
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice(); 
-    let result = playRound(humanChoice, computerChoice);
-    scoreboard = "Round " + round + ": " + playerScore + " Player | " + computerScore + " Computer | " + draw + " draws";
-    console.log(result, scoreboard);
-
-    console.log("Game Over!")
-   
-    function playRound(humanChoice, computerChoice) {    
-        let message = "null";
-        let result = "null";
-
-    //algorithm for determining outomce
-
-        if (humanChoice === "rock") {
-            if (computerChoice === "rock") {
-                result = "draw";
-            } else if (computerChoice === "paper") {
-                result = "lose";
-            } else if (computerChoice === "scissors") {
-                result = "win";
-            }
-        } else if (humanChoice === "paper") {
-            if (computerChoice === "paper") {
-                result = "draw";
-            } else if (computerChoice === "rock") {
-                result = "win";
-            } else if (computerChoice === "scissors") {
-                result = "lose";
-            }
-        } else if (humanChoice === "scissors") {
-            if (computerChoice === "paper") {
-                result = "win";
-            } else if (computerChoice === "rock") {
-                result = "lose";
-            } else if (computerChoice === "scissors") {
-                result = "draw";
-            }
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            result = "draw";
+        } else if (computerChoice === "paper") {
+            result = "lose";
+        } else if (computerChoice === "scissors") {
+            result = "win";
         }
-
-    //UPDATE result based on outcome and update score
-
-        if (result === "draw") {
-            message = "Computer picked " + computerChoice + ". DRAW! " + humanChoice + " and " + computerChoice + " are even.";
-            draw +=1;
-        } else if (result === "win") {
-            message = "Computer picked " + computerChoice + ". You win! " + humanChoice + " beats " + computerChoice + ".";
-            playerScore += 1;
-        } else if (result === "lose") {
-            message = "Computer picked " + computerChoice + ". You lose! " + computerChoice + " beats " + humanChoice + ".";
-            computerScore += 1;
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "paper") {
+            result = "draw";
+        } else if (computerChoice === "rock") {
+            result = "win";
+        } else if (computerChoice === "scissors") {
+            result = "lose";
         }
-        
-        console.log(message);
-
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            result = "win";
+        } else if (computerChoice === "rock") {
+            result = "lose";
+        } else if (computerChoice === "scissors") {
+            result = "draw";
+        }
     }
+    
+    if(result === "win"){humanScore++;}
+    else if(result === "lose"){computerScore++;}
 
+    playerDisplay.textContent = "Player Score: " + humanScore;
+    computerDisplay.textContent = "Computer Score: " + computerScore;
+    message.textContent = "Computer picked " + computerChoice + ". You " + result +".";
+    
+    console.log(humanChoice, computerChoice);
+    console.log(result);
+    console.log(humanScore,computerScore);
 
+    if(humanScore===5){message.textContent = "Game Over, You Win!";
+        humanScore=0;
+        computerScore=0;
+    }
+        else if(computerScore===5){message.textContent = "Game Over, You Lose!";
+            humanScore=0;
+            computerScore=0;
+            
+        }
 }
+
+
+
+
+
 
 
 
